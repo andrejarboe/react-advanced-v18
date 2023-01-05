3fcb74924fa296a8b22d6e987ace507020b957a0
Andre Jarboe <andrejarboe@gmail.com>
2023-01-05 16:08:31 -0500

    useSate object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  });
+
+  const changeMessage = () => {
+    // have to pass in an object because the dat is an object
+    // any key you do not pass in will be wiped out so use ...person
+
+    setPerson({ ...person, message: 'hello world' });
+    // setMessage('hello world')
+  };
+
+  //you could also use different state values:
+  // const [name,setName] = useState('peter')
+  // const [age,setAge] = useState(24)
+  // const [message,setMessage] = useState('random message')
+
+  console.log(person);
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>{person.message}</h4>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
