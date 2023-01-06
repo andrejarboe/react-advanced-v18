0064816f13d30a75cc3c732796a0602ac3b53d8f
Andre Jarboe <andrejarboe@gmail.com>
2023-01-05 20:49:27 -0500

    side effects

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,39 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  /*
+    memory leak:
+    useEffect(() => {
+      console.log('useEffect');
+      window.addEventListener('resize', checkSize);
+    });
+    */
+
+  useEffect(() => {
+    console.log('useEffect');
+    window.addEventListener('resize', checkSize);
+    return () => {
+      console.log('cleanup');
+      // need to clean up when you use side effects
+      window.removeEventListener('resize', checkSize);
+    };
+  }, []);
+
+  console.log('render');
+
+
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size}px</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
