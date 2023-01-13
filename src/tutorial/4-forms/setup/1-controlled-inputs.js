31c4405c8103a4b1faeef9c286d75d18f271158c
Andre Jarboe <andrejarboe@gmail.com>
2023-01-13 17:04:28 -0500

    take input

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -5,28 +5,41 @@ import React, { useState } from 'react';
 // React
 // value, onChange
 
-
 const ControlledInputs = () => {
+	const [firstName, setFirstName] = useState('');
+	const [email, setEmail] = useState('');
 
-  const handleSubmit = (e) => {
-    // stop browser form trying to submit the form 
-    // and refreshing the page
-    e.preventDefault();
-    console.log('helloworld');
-  }
+	const handleSubmit = (e) => {
+		// stop browser form trying to submit the form
+		// and refreshing the page
+		e.preventDefault();
+		console.log(firstName, email);
+	};
 
-  return (
+	return (
 		<article>
 			{/* deal with action on our end */}
 			{/* could also put onClick={handleSubmit} on button  */}
 			<form className="form" onSubmit={handleSubmit}>
 				<div className="form-control">
 					<label htmlFor="firstName">Name: </label>
-					<input type="text" id="firstName" name="firstName" />
+					<input
+						type="text"
+						id="firstName"
+						name="firstName"
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+					/>
 				</div>
 				<div className="form-control">
 					<label htmlFor="email">Email: </label>
-					<input type="text" id="email" name="email" />
+					<input
+						type="text"
+						id="email"
+						name="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
 				</div>
 				<button type="submit">add person</button>
 			</form>
