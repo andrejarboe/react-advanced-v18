a3e1a2172cb6a13c1bbbd23f44ac90cb19c26d8a
Andre Jarboe <andrejarboe@gmail.com>
2023-01-13 16:56:50 -0500

    handle submit notes

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -5,8 +5,33 @@ import React, { useState } from 'react';
 // React
 // value, onChange
 
+
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+
+  const handleSubmit = (e) => {
+    // stop browser form trying to submit the form 
+    // and refreshing the page
+    e.preventDefault();
+    console.log('helloworld');
+  }
+
+  return (
+		<article>
+			{/* deal with action on our end */}
+			{/* could also put onClick={handleSubmit} on button  */}
+			<form className="form" onSubmit={handleSubmit}>
+				<div className="form-control">
+					<label htmlFor="firstName">Name: </label>
+					<input type="text" id="firstName" name="firstName" />
+				</div>
+				<div className="form-control">
+					<label htmlFor="email">Email: </label>
+					<input type="text" id="email" name="email" />
+				</div>
+				<button type="submit">add person</button>
+			</form>
+		</article>
+	);
 };
 
 export default ControlledInputs;
