4cf7a9ba054e49360dbd61718f1ea1d8afc043b8
Andre Jarboe <andrejarboe@gmail.com>
2023-01-13 18:48:52 -0500

    done

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,7 +5,35 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+	const refContainer = useRef(null);
+	const divContainer = useRef(null);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(refContainer.current.value);
+		console.log(divContainer.current);
+	};
+
+  console.log(refContainer);
+  
+  useEffect(() => { 
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  })
+
+	return (
+		<>
+			<form action="" className="form" onSubmit={handleSubmit}>
+				<div className="">
+					<input type="text" ref={refContainer} />
+					<button type="submit">submit</button>
+				</div>
+			</form>
+			<div className="" ref={divContainer}>
+				Hello world
+			</div>
+		</>
+	);
 };
 
 export default UseRefBasics;
